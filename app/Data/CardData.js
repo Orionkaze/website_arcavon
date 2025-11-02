380d00f41f7752f20e9e933a35cc79a9164ec075
Aman Kumar Jha <akjha0810@gmail.com>
2025-11-03 00:30:04 +0530

    Made the Card Carousel

---
+++ app/Data/CardData.js
@@ -0,0 +1,52 @@
+const CardData = [
+  {
+    id: 1, 
+    img: "/cardimg/itachi.jpeg",
+    name: "Itachi Uchiha",
+    description: "A skilled ninja from the Hidden Leaf Village. He is known for his Sharingan eye and tragic backstory.",
+    },
+  {
+    id: 2, 
+    img: "/cardimg/kakashi.jpeg",
+    name: "Kakashi Hatake",
+    description: "The Copy Ninja and leader of Team 7. He is known for his Sharingan eye and laid-back attitude.",
+    },
+  {
+    id: 3, 
+    img: "/cardimg/tanjiro.jpeg",
+    name: "Tanjiro Kamado",
+    description: "A kind-hearted demon slayer with a strong sense of justice. He wields a Nichirin Blade and has a keen sense of smell.",
+    },
+  {
+    id: 4, 
+    img: "/cardimg/tanjiro.jpeg",
+    name: "Tanjiro Kamado",
+    description: "A kind-hearted demon slayer with a strong sense of justice. He wields a Nichirin Blade and has a keen sense of smell.",
+    },
+  {
+    id: 5, 
+    img: "/cardimg/tanjiro.jpeg",
+    name: "Tanjiro Kamado",
+    description: "A kind-hearted demon slayer with a strong sense of justice. He wields a Nichirin Blade and has a keen sense of smell.",
+    },
+  {
+    id: 6, 
+    img: "/cardimg/tanjiro.jpeg",
+    name: "Tanjiro Kamado",
+    description: "A kind-hearted demon slayer with a strong sense of justice. He wields a Nichirin Blade and has a keen sense of smell.",
+    },
+  {
+    id: 7, 
+    img: "/cardimg/tanjiro.jpeg",
+    name: "Tanjiro Kamado",
+    description: "A kind-hearted demon slayer with a strong sense of justice. He wields a Nichirin Blade and has a keen sense of smell.",
+    },
+  {
+    id: 8, 
+    img: "/cardimg/ichigo.jpeg",
+    name: "Ichigo Kurosaki",
+    description: "A high school student who becomes a Soul Reaper. He has the ability to see ghosts and wields a powerful Zanpakuto.",
+    }
+];
+
+export default CardData;
